bb089e0a8ec5c346ae2c7a28f9c93fe21261c874
Shirotsu <albus0sh@gmail.com>
2020-04-06 18:18:35 +0900

    sample build completed

---
+++ Assets/automove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class automove : MonoBehaviour
+{
+    public float offset = 0.0f;
+    public float speed = 0.0f;
+    public float range = 0.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(offset + Mathf.Sin(Time.time * speed) * range,transform.position.y,transform.position.y);
+    }
+}
